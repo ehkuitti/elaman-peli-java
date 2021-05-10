a0138eb9c60e9995592ee8bbb88ef5b5262bd8fa
ehkuitti <eetu.kuittinen@student.laurea.fi>
2021-05-10 16:54:16 +0300

    Projektin aloitus
    Projekti kirjoitetaan Peli.java tiedostoon. Avaa Eclipsellä painamalla Open Project ja valitse sen jälkeen peliprojekti-kansio.

---
+++ peliprojekti/src/pelipaketti/Peli.java
@@ -0,0 +1,13 @@
+/** Projektin nimi on peliprojekti, paketin nimi on pelipaketti ja luokan nimi on Peli. Huomioi
+/ kirjainkoot. **/
+
+package pelipaketti;
+
+public class Peli {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
