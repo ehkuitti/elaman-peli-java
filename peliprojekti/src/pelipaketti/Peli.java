a60b3330bbfaa2020aa3cff15d0b12152e2b37ad
ehkuitti <eetu.kuittinen@student.laurea.fi>
2021-05-28 20:42:51 +0300

    - Kirjoitusvirheiden ja kysymysten korjauksia, tekstin uudelleenformatointia, käyttöohjeiden lisäyksiä

--- peliprojekti/src/pelipaketti/Peli.java
+++ peliprojekti/src/pelipaketti/Peli.java
@@ -23,6 +23,7 @@ public class Peli {
 
 		// Varsinainen koodi alkaa
 		System.out.println("Tervetuloa pelaamaan Tomte™ Corperationin®\n\n\t\tELÄMÄN_PELIÄ©    :)\n");
+		System.out.println("Ohjeet: Vedä aluksi console IDE:ssäsi niin isoksi kuin mahdollista. Tämä helpottaa pelaamista huomattavasti. Tämän jälkeen klikkaa \"Anna nimesi\" -rivin lopusta tyhjältä alueelta.\nVastaa pelissä kirjoittamalla numero tai sana pyydetyn mukaisesti ja painamalla sitten enteriä. Hyviä pelihetkiä toivottaa Tomten väki!\n");
 		System.out.print("Anna nimesi: ");
 		nimi = lukija.nextLine();
 		System.out.println("\n" + nimi + " on hieno nimi!");
@@ -75,7 +76,7 @@ public class Peli {
 	}
 	
 	private static void lopetus() {
-        System.out.println("\n\nSelvisit elämäsi loppuun asti, onnittelut ja pitkää ikää!\n\n\t*VOITIT PELIN*");
+        System.out.println("\nSelvisit elämäsi loppuun asti, onnittelut ja pitkää ikää!\n\n\n\t*VOITIT PELIN*");
 	}
 
 	private static void tentti() {
@@ -86,7 +87,7 @@ public class Peli {
 
 		Scanner laskija = new Scanner (System.in);
 
-	    System.out.println("\nKuinka monta norsulajia on olemassa?\n1) 2\n2) 3\n3) 5");
+	    System.out.println("Kuinka monta norsulajia on olemassa?\n1) 2\n2) 3\n3) 5");
 	    vastaus = laskija.nextInt();
 
 	    if (vastaus == 2) {
@@ -96,14 +97,14 @@ public class Peli {
 	    	System.out.println("\nVäärin! Eikö tämä nyt ole ihan yleistietoa... ¯\\_(ツ)_/¯");
 	    }
 	    
-	    System.out.println("\nKetä Disney-prinsessoista nähdään elokuvissa vähiten? \n1) Ruusunen\n2) Ariel\n3) Lumikki");
+	    System.out.println("\nKetä Disney-prinsessoista nähdään elokuvissa vähiten? \n1) Ruusunen\n2) Jasmine\n3) Lumikki");
 	    vastaus = laskija.nextInt();
 
 	    if (vastaus == 1) {
 	    	System.out.println("\nOikein! Läksyt selvästikin luettu!");
 	    	pistemäärä++;
 	    } else {
-	    	System.out.println("\nVäärin! Matikan kirja sivut 15-78. KERTAA!");
+	    	System.out.println("\nVäärin! Aika katsoa lisää piirreetyjä! =)");
 	    }
 	    System.out.println("\nIlman laskinta: Paljonko on 30% 546:sta? \n1) 163.8\n2) 166.6\n3) 173.95");
 	    vastaus = laskija.nextInt();
@@ -112,25 +113,25 @@ public class Peli {
 	    	System.out.println("\nOikein! Matikkapäätäkin löytyy!");
 	    	pistemäärä++;
 	    } else {
-	    	System.out.println("\nVäärin! Tämähän oli helppo!");
+	    	System.out.println("\nVäärin! Ei kai tämä niin vaikea ollut?");
 	    }
-	    System.out.println("\nMaantietoa: Mikä on Tansanian pääkaupunki? \n1) Mbeya\n2) Dar es Salaam\n3) Dodoma");
+	    System.out.println("\nMaantietoa: Mikä on Yhdysvaltain pääkaupungin *muodollinen* (formaali) nimi? \n1) Washington\n2) Washington, D.C.\n3) District Of Columbia");
 	    vastaus = laskija.nextInt();
 
 	    if (vastaus == 3) {
-	    	System.out.println("\nOikein! Paras kaupunki, heti ennen kotipaikkakuntaasi!");
+	    	System.out.println("\nOikein! Tämä oli paha!");
 	    	pistemäärä++;
 	    } else {
-	    	System.out.println("\nVäärin! Missäs olit Afrikan maantieteen tunnilla, senkin kettu >:( !");
+	    	System.out.println("\nEipä ollu! Washington on osavaltio Yhdysvaltain länsirannikolla ja Washington, D.C. on virallinen *epäformaali* nimi.");
 	    }
         System.out.println("\nMika ainesosa ei kuulu Boulevardier-cocktailiin? \n1) Viski\n2) Campari\n3) Rommi");
 	    vastaus = laskija.nextInt();
 
 	    if (vastaus == 3) {
-	    	System.out.println("\nOikein! Senkin alkoholisti!");
+	    	System.out.println("\nOikein! Tullut useinkin maisteltua?");
 	    	pistemäärä++;
 	    } else {
-	    	System.out.println("\nVäärin! Senkin absolutisti!");
+	    	System.out.println("\nVäärin! Et taida olla alkoholin perään?");
 	    }
 	    
 	    if (pistemäärä == 5) {
@@ -158,7 +159,7 @@ public class Peli {
             System.out.print("\nKuolit");
             peliOhi();
         } else {
-            System.out.print("\nSelvisit. Elämäsi jatkuu. Toistaiseksi...\n\n");
+            System.out.print("\nSelvisit. Elämäsi jatkuu. Toistaiseksi...\n");
             }
 	}
 
@@ -168,21 +169,21 @@ public class Peli {
 		
 		ArrayList<String> lista = new ArrayList<String>();
         String sana;
-        System.out.println("\nMontako sanaa tahdot kirjoittaa?");
+        System.out.println("\nPäätätte kaverisi kanssa kirjoittaa sanoja seinään. Montako sanaa tahdot kirjoittaa?");
         int maara = skanneri.nextInt();
-        System.out.println("\nAnna sanat: ");
+        System.out.println("\nKirjota sanat (paina enteriä sanojen välissä): ");
 
         for (int i = 0; i < maara + 1; i++) {
             sana = skanneri.nextLine();
-            lista.add(sana);
+            lista.add(sana); // halutut sanat listalle tulostusta varten
         }
 
-        System.out.print("\nKirjoitit sanat: ");
+        System.out.print("\nKirjoitit sanat: \n");
         for (String sanoja:lista) {
             System.out.println(sanoja);
             
         }
-		System.out.println("kirjaston seinään.\n\nPoliisi näki tekosi ja määräsi sinulle 800 euroa sakkoja.");
+		System.out.println("\nkirjaston seinään.\n\nPoliisi näki tekosi ja määräsi sinulle 800 euroa sakkoja.");
 	}
 	private static void bileet() {
 
@@ -223,17 +224,17 @@ public class Peli {
 			if (tokaAste > 0 && tokaAste < 4) {
 				switch(tokaAste) {
 					case 1:
-						System.out.println("\nOikein!");
+						System.out.println("\nOikein!\n");
 						tokaTutkinto = "Tiko";
 						opiskeluaika();
 						break;
 					case 2:
-						System.out.println("\nTärkeä ala tämäkin!");
+						System.out.println("\nTärkeä ala tämäkin!\n");
 						tokaTutkinto = "Sosiaali";
 						opiskeluaika();
 						break;
 					case 3:
-						System.out.println("\nEt oo tosissas...");
+						System.out.println("\nEt oo tosissas...\n");
 						tokaTutkinto = "liiketalous";
 						peliOhi();
 				 	}	
@@ -248,7 +249,7 @@ public class Peli {
 
 	private static void peliOhi() {
 		System.out.println("\n\nPeli ohi!\n\nKiitos, kun pelasit!");
-		System.out.println("\n\n\t\t*CREDITS:*\n\nLead Developer: Eetu Kuittinen\nGameplay Designer: Miika Mononen\nSFX editor: Marlo Pietarinen\n\nSpecial thanks to Kaiffari and Jasse\n\n\nAll rights reserved under Tomte™ Corperation®");
+		System.out.println("\n\n\t\t*CREDITS:*\n\n\nLead Developer: Eetu Kuittinen\nGameplay Designer: Miika Mononen\nSFX editor: Marlo Pietarinen\n\nSpecial thanks to Kaiffari and Jasse\n\n\nAll rights reserved under Tomte™ Corperation®");
 		System.exit(0);
 	}
 
@@ -308,13 +309,13 @@ public class Peli {
         if (ekaAste > 0 && ekaAste < 4) {
         	switch (ekaAste) {
 		        case 1:
-			        System.out.println("\nHyvä valinta! Lukiossa järjestetään tentti, jossa kysytään seuraavia asioita:\n");
+			        System.out.println("\nHyvä valinta! Lukiossassi järjestetään tentti, jossa kysytään seuraavia asioita (listalta puuttuvan numeron syöttäminen tulkitaan vääräksi vastaukseksi) :\n");
 			        tentti();
-			        System.out.println("\n Valmistuit ylioppilaaksi ja järjestit huikeat valmistujaisjuhlat!");
+			        System.out.println("\nValmistuit ylioppilaaksi ja järjestit huikeat valmistujaisjuhlat!");
 			        ekaTutkinto = "Lukio";
 		    	break;
 		        case 2:
-		        	System.out.println("\n Hieno valinta! Ammattikoulussa järjestetään tentti, jossa kysytään seuraavia asioita:\n");
+		        	System.out.println("\n Hieno valinta! Ammattikoulussasi järjestetään tentti, jossa kysytään seuraavia asioita (listalta puuttuvan numeron syöttäminen tulkitaan vääräksi vastaukseksi) :\n");
 		        	tentti();
 		        	System.out.println("\nHienoa! Valmistuit haluamaasi ammattiin ja järjestit huikeat valmistujaisjuhlat!");
 		        	ekaTutkinto = "Ammattikoulu";
